cb3e0f256e749bec2a59a99d421be0d990ba18c0
Augustė Bataitytė <auguste.bataityte@ktu.edu>
2026-07-11 16:13:48 +0300

    Day 1 padaryta

---
+++ 01_Day_Introduction/manobandymas.py
@@ -0,0 +1,46 @@
+# Day 1
+
+print(2+3)  # sudetis
+print(3-1)  # atimtis
+print(2*3)  # daugyba
+print(3/2)  # dalyba
+print(3**2) # kelimas laipsniu
+print(3%2)  # liekana
+print(3//2) # dalijimas be liekanos
+
+# Tipu tikrinimas
+print(type(10))                  # int
+print(type(3.14))                # float
+print(type(1+3j))                # complex
+print(type('Asabeneh'))          # string
+print(type([1, 2, 3]))           # list
+print(type({'name':'Asabeneh'})) # dictionary
+print(type({9.8, 3.14, 2.7}))    # set
+print(type((9.8, 3.14, 2.7)))    # tuple
+
+
+# Pratimai
+# Level 1
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+print("Augustė")
+print("Bataitytė")
+print("Lietuva")
+print("I am enjoying 30 days of Python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Augustė'))
+print(type('Bataitytė'))
+print(type('Lietuva'))
+
+# Euklido atstumas tarp dvieju tasku
+print(((10-2)**2 + (8-3)**2) ** 0.5)
