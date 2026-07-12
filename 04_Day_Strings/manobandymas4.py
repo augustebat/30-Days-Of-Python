0c8b56b373ebfc6dc71cc7a74ee7fbedfa58e2fc
Augustė Bataitytė <auguste.bataityte@ktu.edu>
2026-07-12 16:17:28 +0300

    Day 4 Done

--- 04_Day_Strings/manobandymas4.py
+++ 04_Day_Strings/manobandymas4.py
@@ -86,3 +86,75 @@ print(sakinys4.find('because'))
 
 #24 Use rindex to find the position of the last occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
 print(sakinys4.rindex('because'))
+
+#25 Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+pradzios_indeksas = sakinys4.find('because')
+pabaigos_indeksas = sakinys4.rfind('because')
+print(sakinys4[pradzios_indeksas:pabaigos_indeksas+7])
+
+#26 Find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+# kartojasi
+
+#27 Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+# kartojasi
+
+#28 Does 'Coding For All' start with a substring Coding?
+print(tekstas2.startswith('Coding'))
+
+#29 Does 'Coding For All' end with a substring coding?
+print(tekstas2.endswith('coding'))
+
+#30 '   Coding For All      '  , remove the left and right trailing spaces in the given string.
+tekstas4 = '   Coding For All      '
+print(tekstas4.strip())
+
+#31 Which one of the following variables return True when we use the method isidentifier():
+#30DaysOfPython
+#thirty_days_of_python
+variable1 = '30DaysOfPython'
+variable2 = 'thirty_days_of_python'
+print(variable1.isidentifier())
+print(variable2.isidentifier())
+
+#32 The following list contains the names of some of python libraries: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Join the list with a hash with space string.
+libraries = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+print('# '.join(libraries))
+
+#33 Use the new line escape sequence to separate the following sentences.
+print('I am enjoying this challenge.\nI just wonder what is next.')
+
+#34 Use a tab escape sequence to write the following lines.
+#Name      Age     Country   City
+#Asabeneh  250     Finland   Helsinki
+print('Name\tAge\tCountry\tCity')
+print('Asabeneh\t250\tFinland\tHelsinki')
+
+#35 Use the string formatting method to display the following:
+#radius = 10
+#area = 3.14 * radius ** 2
+#The area of a circle with radius 10 is 314 meters square.
+radius = 10
+area = 3.14 * radius ** 2
+
+print(f"The area of a circle with radius {radius} is {int(area)} meters square.")
+print("The area of a circle with radius {} is {} meters square.".format(radius, int(area)))
+
+#36 Make the following using string formatting methods:
+"""8 + 6 = 14
+8 - 6 = 2
+8 * 6 = 48
+8 / 6 = 1.33
+8 % 6 = 2
+8 // 6 = 1
+8 ** 6 = 262144"""
+
+a = 8
+b = 6
+
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b:.2f}")  # :.2f palieka tik du skaičius po kablelio
+print(f"{a} % {b} = {a % b}")
+print(f"{a} // {b} = {a // b}")
+print(f"{a} ** {b} = {a ** b}")
