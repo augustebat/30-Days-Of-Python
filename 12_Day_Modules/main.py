ae4ae7263d45cc75aea11c27a8fa138021c23ba1
Augustė Bataitytė <auguste.bataityte@ktu.edu>
2026-07-24 17:54:02 +0300

    Day 12 done

--- 12_Day_Modules/main.py
+++ 12_Day_Modules/main.py
@@ -1,5 +1,5 @@
+from mymodule import generate_full_name as fullname, sum_two_nums as total, person as p, gravity as g, random_user_id, user_id_gen_by_user, rgb_color_gen, list_of_hexa_colors, list_of_rgb_colors, generate_colors, shuffle_list as shuffle, seven_random
 
-from mymodule import generate_full_name as fullname, sum_two_nums as total, person as p, gravity as g
 print(fullname('Asabneh','Yetayeh'))
 print(total(1, 9))
 mass = 100
@@ -8,3 +8,45 @@ weight = mass * g
 print(weight)
 print(p)
 print(p['firstname'])
+
+#-----------Level 1-----------
+#1 Write a function which generates a six digit/character random_user_id.
+print(random_user_id()) 
+
+#2 Modify the previous task. 
+# Declare a function named user_id_gen_by_user. 
+# It doesn’t take any parameters but it takes two inputs using input(). 
+# One of the inputs is the number of characters and the second input is the number of IDs which are supposed to be generated.
+print(user_id_gen_by_user()) # user input: 5 5
+#print(user_id_gen_by_user()) # 16 5
+
+#3 Write a function named rgb_color_gen. It will generate rgb colors (3 values ranging from 0 to 255 each).
+# rgb(125,244,255) - the output should be in this form
+print(rgb_color_gen())
+
+
+
+#-----------Level 2-----------
+#4 Write a function list_of_hexa_colors which returns any number of hexadecimal colors in an array 
+# (six hexadecimal numbers written after #. Hexadecimal numeral system is made out of 16 symbols, 0-9 and first 6 letters of the alphabet, a-f. Check the task 6 for output examples).
+print(list_of_hexa_colors(3))
+
+#5 Write a function list_of_rgb_colors which returns any number of RGB colors in an array.
+print(list_of_rgb_colors(2))
+
+#6 Write a function generate_colors which can generate any number of hexa or rgb colors.
+
+print(generate_colors('hexa', 3)) # ['#a3e12f', '#03ed55', '#eb3d2b']
+print(generate_colors('hexa', 1)) # ['#b334ef']
+print(generate_colors('rgb', 3))  # ['rgb(5, 55, 175)', 'rgb(50, 105, 100)', 'rgb(15, 26, 80)']
+print(generate_colors('rgb', 1))  # ['rgb(33, 79, 176)']
+
+
+
+#-----------Level 3-----------
+#7 Call your function shuffle_list, it takes a list as a parameter and it returns a shuffled list
+names = ['Abigail', 'Bella', 'John', 'Mary']
+print(shuffle(names))
+
+#8 Write a function which returns an array of seven random numbers in a range of 0-9. All the numbers must be unique.
+print(seven_random())
--- 12_Day_Modules/mymodule.py
+++ 12_Day_Modules/mymodule.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 def generate_full_name(firstname, lastname):
       space = ' '
       fullname = firstname + space + lastname
@@ -13,4 +16,83 @@ person = {
     "city":'Helsinki'
 }
 
+#-----------Level 1-----------
+#1 Write a function which generates a six digit/character random_user_id.
+def random_user_id():
+    characters = string.ascii_letters + string.digits
+    user_id = ''.join(random.choices(characters, k=6))
+    return user_id
 
+#2 Modify the previous task. 
+# Declare a function named user_id_gen_by_user. 
+# It doesn’t take any parameters but it takes two inputs using input(). 
+# One of the inputs is the number of characters and the second input is the number of IDs which are supposed to be generated.
+def user_id_gen_by_user():
+    num_of_characters = int(input('Enter the number of characters: '))
+    num_of_ids = int(input('Enter the number of IDs to generate: '))
+    characters = string.ascii_letters + string.digits
+
+    ids = []
+    for i in range(0, num_of_ids):
+        ids.append(''.join(random.choices(characters, k=num_of_characters)))
+    return '\n'.join(ids)
+
+
+#3 Write a function named rgb_color_gen. It will generate rgb colors (3 values ranging from 0 to 255 each).
+def rgb_color_gen():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return f"rgb({r}, {g}, {b})"
+
+#-----------Level 2-----------
+#4 Write a function list_of_hexa_colors which returns any number of hexadecimal colors in an array 
+# (six hexadecimal numbers written after #. Hexadecimal numeral system is made out of 16 symbols, 0-9 and first 6 letters of the alphabet, a-f. Check the task 6 for output examples).
+def list_of_hexa_colors(num):
+    characters = string.hexdigits.lower()
+    hex_codes = []
+    for _ in range(num):
+        hexa = ''.join(random.choices(characters, k=6))
+        code = '#' + hexa
+        hex_codes.append(code)
+    return hex_codes
+
+#5 Write a function list_of_rgb_colors which returns any number of RGB colors in an array.
+def list_of_rgb_colors(num):
+    rgb_codes = []
+    for _ in range(num):
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        coder = f"rgb({r}, {g}, {b})"
+        rgb_codes.append(coder)
+    return rgb_codes
+
+
+#6 Write a function generate_colors which can generate any number of hexa or rgb colors.
+"""
+   generate_colors('hexa', 3) # ['#a3e12f','#03ed55','#eb3d2b'] 
+   generate_colors('hexa', 1) # ['#b334ef']
+   generate_colors('rgb', 3)  # ['rgb(5, 55, 175','rgb(50, 105, 100','rgb(15, 26, 80'] 
+   generate_colors('rgb', 1)  # ['rgb(33,79, 176)']
+"""
+    
+def generate_colors(type, num):
+    if type == 'hexa':
+        return list_of_hexa_colors(num)
+    elif type == 'rgb':
+        return list_of_rgb_colors(num)
+    else:
+        return "Neteisingas tipas! Naudokite 'hexa' arba 'rgb'."
+    
+
+#-----------Level 3-----------
+#7 Call your function shuffle_list, it takes a list as a parameter and it returns a shuffled list
+def shuffle_list(lst):
+    if type(lst) is list:
+        new_lst = random.shuffle(lst)
+    return lst
+
+#8 Write a function which returns an array of seven random numbers in a range of 0-9. All the numbers must be unique.
+def seven_random():
+    return random.sample(range(10), k=7)
